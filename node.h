338ec92c25b4ec338843a375d625ab5953625c9d
Maramal <55010533+Maramall@users.noreply.github.com>
2021-02-20 13:42:40 +0700

    Create node.h

---
+++ node.h
@@ -0,0 +1,21 @@
+#ifndef node_H
+#define node_H
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	string word;
+	int count;
+	Node *next; 
+};
+typedef Node *PNode; // тип меняем на указатель, я не понимаю, как это работает
+PNode CreateNode (string nword);
+void prov(string &a);
+void AddFirst (PNode &Head, PNode NewNode);
+void AddAfter (PNode p, PNode NewNode);
+void AddBefore(PNode &Head, PNode p, PNode NewNode);
+void AddLast(PNode &Head, PNode NewNode);
+PNode Find (PNode Head, string NewWord);
+PNode FindPlace (PNode Head, string NewWord);
+
+#endif
